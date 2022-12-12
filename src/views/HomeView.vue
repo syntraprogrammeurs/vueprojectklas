4d54cdebdddd37827931ae3ccde547a478471011
Tom <syntraprogrammeurs@gmail.com>
2022-12-12 20:16:16 +0100

    home page cards aangepast

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <TicketItem v-for="event in events" :key="event.id" :event="event" />
+  <div class="home container-fluid">
+    <div class="row">
+      <div class="col-12 col-lg-10 offset-lg-1">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <div class="row">
+          <TicketItem v-for="event in events" :key="event.id" :event="event" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
