a81889512f78c20361bf6332eda07ef8885d1019
Tom <syntraprogrammeurs@gmail.com>
2022-12-12 19:25:04 +0100

    working PortfolioView with PortfolioItem component integrated

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container-fluid">
+    <div class="col-12 col-lg-6 offset-lg-3">
+      <ul class="list-group mb-3">
+        <li
+          class="list-group-item"
+          :class="[werk.id % 2 ? 'bg-danger text-white' : 'bg-dark text-white']"
+        >
+          {{ werk.id }} - {{ werk.title }}
+        </li>
+        <li class="list-group-item">Category:{{ werk.category }}</li>
+        <li class="list-group-item">{{ werk.description }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PortfolioItem",
+  props: {
+    werk: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="portfolio">
-    <h1>This is a portfolio page</h1>
+    <PortfolioItem v-for="werk in werken" :key="werk.id" :werk="werk" />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+import PortfolioItem from "@/components/PortfolioItem";
+
+export default {
+  name: "PortfolioView",
+  data() {
+    return {
+      werken: [
+        {
+          id: 1,
+          category: "webdesign",
+          title: "Proxpace webdesign",
+          description: "Proxpace webdesign in photoshop/illustrator",
+        },
+        {
+          id: 2,
+          category: "vue",
+          title: "Kassasysteem",
+          description:
+            "Een kassasysteem opgebouwd met vue component zonder @vue/cli. De bedoeling is om hier een notie te krijgen van de standaard onderdelen van het basis vue component. Onderdelen zoals: data, methods, computed, ...",
+        },
+        {
+          id: 3,
+          category: "bootstrap",
+          title: "eindwerk responsive webshop",
+          description:
+            "De bedoeling hier is om alle pagina's af te werken voor een STATISCHE RESPONSIVE webshop tegen begin januari 2023",
+        },
+      ],
+    };
+  },
+  components: {
+    PortfolioItem,
+  },
+};
+</script>
